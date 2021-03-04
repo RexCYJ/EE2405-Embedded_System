558fc5bbdd4934055bf5f9fb692e70d271c62600
RexCYJ <rexchen2001@gmail.com>
2021-03-04 10:24:18 -0800

    add mbed04 codes

---
+++ mbed04/4_4_FFT_analysis/main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+
+AnalogOut Aout(D7);
+AnalogIn Ain(A0);
+
+int sample = 128;
+int i;
+
+float ADCdata[128];
+
+int main(){
+  while (1) {
+    for (i = 0; i < sample; i++){
+      Aout = Ain;
+      ADCdata[i] = Ain;
+      ThisThread::sleep_for(1000ms/sample);
+    }
+    for (i = 0; i < sample; i++){
+      printf("%f\r\n", ADCdata[i]);
+      ThisThread::sleep_for(100ms);
+    }
+  }
+}
